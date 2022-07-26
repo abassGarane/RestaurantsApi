8ef522208367dd6f9311531dfaaeb6a770e3c7cd
abassGarane <77203205+abassGarane@users.noreply.github.com>
2022-07-26 12:56:37 +0300

    Update secrets.ts

--- src/config/secrets.ts
+++ src/config/secrets.ts
@@ -1,4 +1,1 @@
-export const ACCOUNTSID = 'AC6de88ce26d30c146174d7aa9691f8ec1'
-export const AUTHTOKEN = '063fe3f358c576cb0781fb7a774d0f90'
-export const MONGO_URI =
-	'mongodb+srv://philos:guru@cluster0.uawkvmd.mongodb.net/food?retryWrites=true&w=majority'
+
